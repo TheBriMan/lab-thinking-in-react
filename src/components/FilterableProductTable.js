e6cdbbf7b07ef58cbf94e85095e53ecea358ff42
Brian Dowd <dowdbrian@yahoo.com>
2021-08-06 13:18:47 +0200

    done, cannot run app, issue with dependencies

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import SearchBar from './components/SearchBar.js';
+import ProductTable from './components/ProductTable.js';
+
+class FilterableProductTable extends Component {
+render() {
+        <SearchBar />
+        <ProductTable />
+    }
+}
+
+export default FilterableProductTable
---
+++ src/components/ProductRow.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ProductRow extends Component {
+    render () {
+        <>
+        </>
+    }
+}
+
+export default ProductRow;
